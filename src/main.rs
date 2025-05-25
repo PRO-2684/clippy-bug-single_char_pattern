457e169d2f1e5cf22b8af8e7089380e51e3f7688
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-05-25 16:41:58 +0800

    feat: Bug repro success

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 #![warn(clippy::single_char_pattern)]
 
-use std::num::ParseIntError;
+use std::{num::ParseIntError, str::Split};
 
 fn main() -> Result<(), ParseIntError> {
     let time = String::from("12:34:56");
-    let mut parts = time.split(":");
+    let seconds = time_to_seconds(&time)?;
+    println!("{time} is {seconds}s");
+
+    Ok(())
+}
+
+fn time_to_seconds(time: &str) -> Result<u32, ParseIntError> {
+    let mut parts: Split<'_, &str> = time.split(":");
 
     let hours = parts.next().map_or(Ok(0), str::parse)?;
     let minutes = parts.next().map_or(Ok(0), str::parse)?;
     let mut seconds = parts.next().map_or(Ok(0), str::parse)?;
     seconds += minutes * 60 + hours * 3600;
 
-    println!("{time} is {seconds}s");
-    Ok(())
+    Ok(seconds)
 }
